129fd623f8d2256232dae6ea3d78ac925ed54aa8
Sunil Thapa <sunil.thapa@icp.edu.np>
2026-01-27 14:25:22 +0545

    Update Topic.java

--- src/Topic.java
+++ src/Topic.java
@@ -4,18 +4,12 @@ import java.util.List;
 public class Topic {
 
     private String name;
-    private List<String> entries;
 
     public Topic(String name) {
         this.name = name;
-        this.entries = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
-
-    // TODO: Add a method to add an entry
-
-    // TODO: Add a method to return all entries
 }
